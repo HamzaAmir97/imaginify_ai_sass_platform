7ab39266f2a28ff2448212a4a2862139e3e6a66c
Hmazah <alhamza2012@gmail.com>
2024-12-23 10:00:00 +0200

    add  complete route structure folder

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TransofrmationsPage = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default TransofrmationsPage
---
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UpdateTransofrmationPage = () => {
+  return (
+    <div>UpdateTransofrmationPage</div>
+  )
+}
+
+export default UpdateTransofrmationPage
