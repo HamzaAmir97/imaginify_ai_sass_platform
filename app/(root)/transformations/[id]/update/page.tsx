2ecde9ab3064ca63d2e6e06c0908bbfe03112c79
Hmazah <alhamza2012@gmail.com>
2025-06-11 12:00:00 +0300

    add  update image form

--- app/(root)/transformations/[id]/update/page.tsx
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { redirect } from "next/navigation";
 
-const UpdateTransofrmationPage = () => {
+import Header from "@/components/shared/Header";
+import { transformationTypes } from "@/constants";
+import { getImageById } from "@/lib/actions/image.actions";
+import { auth } from "@clerk/nextjs/server";
+import { getUserById } from "@/lib/actions/user.action";
+import TransformationForm from "@/components/shared/TransformationForm ";
+
+const Page = async ({ params }: { params: { id: string } }) => {
+  const { id } = params;
+  const { userId } = await auth();
+
+  if (!userId) redirect("/sign-in");
+
+  const user = await getUserById(userId);
+  const image = await getImageById(id);
+
+  const transformation =
+    transformationTypes[image.transformationType as TransformationTypeKey];
+
   return (
-    <div className='text-red-300'>UpdateTransofrmationPage</div>
-  )
-}
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
 
-export default UpdateTransofrmationPage
+      <section className="mt-10">
+        <TransformationForm
+          action="Update"
+          userId={user._id}
+          type={image.transformationType as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+          config={image.config}
+          data={image}
+        />
+      </section>
+    </>
+  );
+};
+
+export default Page;
--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,23 @@ const nextConfig: NextConfig = {
   // typescript :{
   //   ignoreBuildErrors :true
   // },
+
   eslint :{
    ignoreDuringBuilds :true
   },
+  images :{
+    remotePatterns:[
+     {
+
+      protocol:"https",
+      hostname:'res.cloudinary.com',
+      port: ''
+
+     }
+
+  ]
+
+  }
 };
 
 export default nextConfig;
