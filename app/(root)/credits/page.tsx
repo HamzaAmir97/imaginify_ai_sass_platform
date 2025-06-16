8b9cc5e138d85cbb7d7f66aa026f9a51ba830101
Hmazah <alhamza2012@gmail.com>
2025-06-16 12:00:00 +0300

    add stripe  functionality

--- app/(root)/credits/page.tsx
+++ app/(root)/credits/page.tsx
@@ -1,15 +1,16 @@
-import { SignedIn, auth } from "@clerk/nextjs";
+import { SignedIn,  } from "@clerk/nextjs";
 import Image from "next/image";
 import { redirect } from "next/navigation";
 
 import Header from "@/components/shared/Header";
 import { Button } from "@/components/ui/button";
 import { plans } from "@/constants";
-import { getUserById } from "@/lib/actions/user.actions";
+import { getUserById } from "@/lib/actions/user.action";
 import Checkout from "@/components/shared/Checkout";
+import { auth } from "@clerk/nextjs/server";
 
 const Credits = async () => {
-  const { userId } = auth();
+  const { userId } = await auth();
 
   if (!userId) redirect("/sign-in");
 
